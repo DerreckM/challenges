56acacac4d482ef1a95e02ecf708e9f60f9e1311
derreck <mansheim23@hotmail.com>
2016-03-04 09:46:28 -0600

    yield_two through yield_ten pass

--- lib/block_challenges.rb
+++ lib/block_challenges.rb
@@ -27,36 +27,109 @@ def yield_first(array, &block)
   end
 end
 
-def yield_em_all(array, &block)
-
-end
+#def yield_em_all(array, &block)
+#  block.call(array)
+#end
 
 def yield_two(array, &block)
+  block.call(array[0])
+  block.call(array[1])
 end
 
 def yield_three(array, &block)
+  block.call(array[0])
+  block.call(array[1])
+  block.call(array[2])
 end
 
 def yield_four(array, &block)
+  #block.call(array[0..3])     =>  This fails--- expected ["a", "b", "c", "d"] got: [["a", "b", "c", "d"]]
+  if array == []
+    []
+  else
+    i = 0
+    while i < array.length
+      block.call(array[i])
+      i += 1
+    end
+  end
 end
 
 def yield_five(array, &block)
+  if array == []
+    []
+  else
+    i = 0
+    while i < array.length
+      block.call(array[i])
+      i += 1
+    end
+  end
 end
 
+
 def yield_six(array, &block)
+  if array == []
+    []
+  else
+    i = 0
+    while i < array.length
+      block.call(array[i])
+      i += 1
+    end
+  end
 end
 
 def yield_seven(array, &block)
+  if array == []
+    []
+  else
+    i = 0
+    while i < array.length
+      block.call(array[i])
+      i += 1
+    end
+  end
 end
 
 def yield_eight(array, &block)
+  if array == []
+    []
+  else
+    i = 0
+    while i < array.length
+      block.call(array[i])
+      i += 1
+    end
+  end
 end
+
 
 def yield_nine(array, &block)
+  if array == []
+    []
+  else
+    i = 0
+    while i < array.length
+      block.call(array[i])
+      i += 1
+    end
+  end
 end
 
+
 def yield_ten(array, &block)
+  if array == []
+    []
+  else
+    i = 0
+    while i < array.length
+      block.call(array[i])
+      i += 1
+    end
+  end
 end
+
 
 def yield_with_while_loop(array, &block)
 end
